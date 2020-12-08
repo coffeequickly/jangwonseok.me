37b45068365f66c0343d77f98d7b64e4f7b2b9cd
--global <jangwonseok.js@gmail.com>
2020-12-09 01:49:19 +0900

    css 리셋 추가, 전역 css 생성 등

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,11 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'normalize.css/normalize.css',
+    {
+      src: '~/assets/common.scss',
+      lang: 'scss'
+    }
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
