652638e907d8079ad086a04bfc22a8ca419369ce
--global <jangwonseok.js@gmail.com>
2020-12-10 16:48:11 +0900

    ga 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,9 @@ export default {
     ],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [],
+    plugins: [
+        {src : '~plugins/googleAnalytics.js', mode: 'client'}
+    ],
 
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
