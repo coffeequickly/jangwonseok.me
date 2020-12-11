415118c92c203c68ec7666bb9e244275bb20797d
--global <jangwonseok.js@gmail.com>
2020-12-11 13:50:47 +0900

    관리자 페이지 이동 처리

---
+++ pages/admin.vue
@@ -0,0 +1,16 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+    name: "admin",
+    beforeCreate() {
+        location.href = "https://api.jangwonseok.me/wp-admin"
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
