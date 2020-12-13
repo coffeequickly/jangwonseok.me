c4a0e6bbfd64a1c0b307dfdb2093e89ea68d3e5b
--global <jangwonseok.js@gmail.com>
2020-12-13 14:24:49 +0900

    카테고리 대응

--- pages/_category/_postId.vue
+++ pages/_category/_postId.vue
@@ -1,15 +1,6 @@
 <template>
     <article v-if="this.$store.state.posts.post">
-        포스트 타입 : {{ this.$route.params.category }}<br/>
-        포스트 ID : {{ this.$route.params.postId }}<br/>
-        <div v-if="this.$store.state.posts.post" v-html="this.$store.state.posts.post.content.rendered"  class="render"></div>
-        <div v-if="this.$store.state.posts.postList">
-            <ul>
-                <li v-for="(list, index) in this.$store.state.posts.postList" :key="index">
-                    {{list}}
-                </li>
-            </ul>
-        </div>
+        <div v-html="this.$store.state.posts.post.content.rendered"  class="render"></div>
     </article>
 
     <ul v-else-if="this.$store.state.posts.postList">
@@ -25,11 +16,9 @@ export default {
     // layout: (route) => route.params.postId ? 'post-article' : 'post-list',
     layout: 'post',
     mounted() {
-
-
         this.$store.dispatch('posts/getPost', {
-            postId: this.$route.params.postId,
-            // postType: 'episode'
+            category : this.$route.params.postId ? null : this.$route.params.category,
+            postId: this.$route.params.postId
         });
     }
 }
--- store/posts.js
+++ store/posts.js
@@ -1,6 +1,7 @@
 export const state = () => ({
     postList : [],
-    post : null
+    post : null,
+    categories : []
 })
 
 export const mutations = {
@@ -10,27 +11,52 @@ export const mutations = {
 
     SET_POST(state, payload){
         state.post = payload;
+    },
+    SET_CATEGORIES(state, payload){
+        state.categories = payload;
     }
 }
 
 export const actions = {
-    async getPost({commit}, {postType, postId}){
-        console.log(postId);
+    async getCategories({commit}){
+        await $nuxt.fetchAPI('GET', '/categories').then(result =>{
+            let categoryArrange = {};
+            result.data.forEach(item => {
+                categoryArrange[item.slug] = item.id;
+            })
 
-        let endpoint = '/posts/';
+            commit('SET_CATEGORIES', categoryArrange);
+        });
+    },
 
-        if(postId){
-            endpoint += postId;
-        }
 
-        // TODO : postType 분리되면 거기 맞게 endpoint 분기처리 해주어야 함.
+    async getPost({state, commit, dispatch}, {category, postId}){
 
-        await $nuxt.fetchAPI('GET', endpoint).then(result => {
-            if(postId){
-                commit('SET_POST', result.data);
-            }else{
-                commit('SET_LIST', result.data);
+        await dispatch('getCategories').then(() =>{
+            commit('SET_POST', null);
+            commit('SET_LIST', []);
+
+            let endpoint = '/posts/';
+
+            console.log(category);
+
+            if(category){
+                endpoint += '?categories=' + state.categories[category];
             }
+
+            if(postId){
+                endpoint += postId;
+            }
+
+            // TODO : postType 분리되면 거기 맞게 endpoint 분기처리 해주어야 함.
+
+            $nuxt.fetchAPI('GET', endpoint).then(result => {
+                if(postId){
+                    commit('SET_POST', result.data);
+                }else{
+                    commit('SET_LIST', result.data);
+                }
+            })
         })
     },
 }
