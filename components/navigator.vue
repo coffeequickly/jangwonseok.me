c606c1d8c553726d02c9e4a1cfd7be559d2bd1e1
--global <jangwonseok.js@gmail.com>
2020-12-13 16:21:46 +0900

    메뉴 네비게이터 분리(모바일 대응 때문에 공용으로)

---
+++ components/navigator.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav>
+        <nuxt-link to="/" v-slot="{ href, navigate, isActive, isExactActive }">
+            <a :href="href" @click="navigate" :class="{'is-active' : isExactActive}">대문</a>
+        </nuxt-link>
+
+        <nuxt-link to="/logs" v-slot="{ href, navigate, isActive, isExactActive }">
+            <a :href="href" @click="navigate" :class="{'is-active' : isActive}">개발괴발</a>
+        </nuxt-link>
+
+        <nuxt-link to="/article" v-slot="{ href, navigate, isActive, isExactActive }">
+            <a :href="href" @click="navigate" :class="{'is-active' : isActive}">탐구생활</a>
+        </nuxt-link>
+
+        <nuxt-link to="/episode" v-slot="{ href, navigate, isActive, isExactActive }">
+            <a :href="href" @click="navigate" :class="{'is-active' : isActive}">고민고민</a>
+        </nuxt-link>
+
+        <nuxt-link to="/about" v-slot="{ href, navigate, isActive, isExactActive }">
+            <a :href="href" @click="navigate" :class="{'is-active' : isActive}">장원석에 대해</a>
+        </nuxt-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "navigator"
+}
+</script>
+
+<style scoped lang="scss">
+@import 'assets/partialAsset';
+
+a{
+    margin-left:8px;
+    color:$color-green-main;
+    display:inline-block;
+    padding:4px 16px;
+    box-sizing: border-box;
+    text-decoration: none;
+    transition: all 0.3s ease-in-out;
+
+&:before{
+     content:'# '
+ }
+
+&:hover, &.is-active{
+              background-color: $color-green-main;
+              color:$color-light-100;
+              transition: all 0.3s ease-in-out;
+          }
+}
+</style>
