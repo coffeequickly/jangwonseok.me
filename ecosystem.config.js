8b365a30781b90ac4f2039dd97462381bc339a27
--global <jangwonseok.js@gmail.com>
2020-12-28 01:39:20 +0900

    포트 제거

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,7 @@ module.exports = {
             exec_mode: 'cluster',
             instances: '2', // Or a number of instances
             script: './node_modules/nuxt/bin/nuxt.js',
-            args: 'start',
-            env_production: {
-                NODE_ENV: 'production',
-                PORT: '3000',
-            },
+            args: 'start'
         }
     ]
 }
